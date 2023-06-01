54817b6209918e02991a7ef4b896f6501b60687f
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2023-06-01 12:37:54 -0400

    Add basic navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import "./navigation.styles.scss";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container" to="/shop">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;

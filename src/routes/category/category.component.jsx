6eaca3b6e7a85fe6aa62e6c34b2fe4051a712571
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2023-06-08 22:49:06 -0400

    Add category previews and category pages

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,30 @@
+import "./category.styles.scss";
+
+import { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { CategoriesContext } from "../../contexts/categories.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+  return (
+    <>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => {
+            return <ProductCard product={product} key={product.id} />;
+          })}
+      </div>
+    </>
+  );
+};
+
+export default Category;

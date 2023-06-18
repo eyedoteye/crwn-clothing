7defcd278beccae533ca8e4cc16d9c7b7efa8350
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2023-06-17 22:07:34 -0400

    Move loggerMiddleware to logger.js

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,12 @@
+export const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  console.log(action.type);
+  console.log(action.payload);
+  console.log("current state: ", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
--- src/store/store.js
+++ src/store/store.js
@@ -5,19 +5,6 @@ import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
-// const loggerMiddleware = (store) => (next) => (action) => {
-//   if (!action.type) {
-//     return next(action);
-//   }
-//   console.log(action.type);
-//   console.log(action.payload);
-//   console.log("current state: ", store.getState());
-
-//   next(action);
-
-//   console.log("next state: ", store.getState());
-// };
-
 const persistConfig = {
   key: "root",
   storage,
@@ -26,9 +13,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
